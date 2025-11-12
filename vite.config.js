619768b7fc580e5aeb63c327d539ad4d1dbf0f02
nansotegi <nansotegi@gmail.com>
2025-11-12 17:33:25 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: set your repo name here, exactly as in GitHub
-  base: '/puzzle/',
-  build: { outDir: 'docs' }
+  base: '/puzzle/'
 })
